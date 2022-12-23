1c5e17e8260e9014dc87b9667d51ef9e3871ef8e
Francis <mfrncis794@gmail.com>
2022-12-23 17:41:27 -0500

    test: add mock MedicalRecordsService

--- src/app/core/services/medical-records.service.spec.ts
+++ src/app/core/services/medical-records.service.spec.ts
@@ -1,13 +1,24 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { MedicalRecordsService } from './medical-records.service';
+import { UsersService } from './users.service';
 
 describe('MedicalRecordsService', () => {
   let service: MedicalRecordsService;
+  let usersService: UsersService;
+  let controller: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UsersService],
+    });
+
     service = TestBed.inject(MedicalRecordsService);
+    usersService = TestBed.inject(UsersService);
+    controller = TestBed.inject(HttpTestingController);
+
   });
 
   it('should be created', () => {
