ac22e0e3a345983c0c59996a6cd8fa803b482a08
Francis <mfrncis794@gmail.com>
2022-12-23 16:50:01 -0500

    test: add mock store

--- src/app/modules/main/pages/add-user/add-user.component.spec.ts
+++ src/app/modules/main/pages/add-user/add-user.component.spec.ts
@@ -1,20 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { AddUserComponent } from './add-user.component';
 
 describe('AddUserComponent', () => {
   let component: AddUserComponent;
   let fixture: ComponentFixture<AddUserComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddUserComponent ]
+      declarations: [ AddUserComponent ],
+      providers: [provideMockStore({ })],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(AddUserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    store = TestBed.inject(MockStore);
   });
 
   it('should create', () => {
--- src/app/modules/main/pages/clinical-history/clinical-history.component.spec.ts
+++ src/app/modules/main/pages/clinical-history/clinical-history.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { ClinicalHistoryComponent } from './clinical-history.component';
 import { ClinicalHistoryTableColumns } from './clinical-history.config';
@@ -6,16 +7,20 @@ import { ClinicalHistoryTableColumns } from './clinical-history.config';
 describe('ClinicalHistoryComponent', () => {
   let component: ClinicalHistoryComponent;
   let fixture: ComponentFixture<ClinicalHistoryComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ClinicalHistoryComponent ]
+      declarations: [ ClinicalHistoryComponent ],
+      providers: [provideMockStore({ })],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ClinicalHistoryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    store = TestBed.inject(MockStore);
   });
 
   it('should create', () => {
