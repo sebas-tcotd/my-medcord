9cbcf13a26c0158619b9859f253206571a7eb86d
Francis <mfrncis794@gmail.com>
2022-12-24 09:32:19 -0500

    test: add test components

--- src/app/core/services/appointment.service.spec.ts
+++ src/app/core/services/appointment.service.spec.ts
@@ -1,13 +1,26 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { AppointmentService } from './appointment.service';
+import { UsersService } from './users.service';
 
 describe('AppointmentService', () => {
   let service: AppointmentService;
+  let usersService: UsersService;
+  let controller: HttpTestingController;
+  
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UsersService],
+    });
     service = TestBed.inject(AppointmentService);
+    usersService = TestBed.inject(UsersService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
--- src/app/modules/main/pages/create-appointment/create-appointment.component.spec.ts
+++ src/app/modules/main/pages/create-appointment/create-appointment.component.spec.ts
@@ -1,20 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { CreateAppointmentComponent } from './create-appointment.component';
 
 describe('CreateAppointmentComponent', () => {
   let component: CreateAppointmentComponent;
   let fixture: ComponentFixture<CreateAppointmentComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateAppointmentComponent ]
+      declarations: [ CreateAppointmentComponent ],
+      providers: [provideMockStore({ })],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(CreateAppointmentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    store = TestBed.inject(MockStore);
   });
 
   it('should create', () => {
